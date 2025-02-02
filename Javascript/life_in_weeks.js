e10d91b3c0cf7af3fe27c805357a7aeae3508dbc
Alejandro Real32 <alejandro.real@upnay.edu.mx>
2025-02-02 00:49:53 -0700

    Trabajo_en_equipo_css

--- Javascript/life_in_weeks.js
+++ Javascript/life_in_weeks.js
@@ -7,7 +7,6 @@ You have x days, y weeks, and z months left.
 
 Where x, y and z are replaced with the actual calculated numbers.
 
-
 For this challenge, assume there are 365 days in a year, 52 weeks in a year and 12 months in a year.
 
 IMPORTANT your console.log output should match the Example Output format exactly, even the positions of the commas and full stops.
@@ -23,18 +22,18 @@ Example Output
     You have 12410 days, 1768 weeks, and 408 months left.
 */
 
-
 function lifeInWeeks(age) {
+    /************Don't change the code above************/    
     
-/************Don't change the code above************/    
+    const maxAge = 90;
+    const yearsLeft = maxAge - age;
+    const daysLeft = yearsLeft * 365;
+    const weeksLeft = yearsLeft * 52;
+    const monthsLeft = yearsLeft * 12;
     
-    //Write your code here.
+    console.log(`You have ${daysLeft} days, ${weeksLeft} weeks, and ${monthsLeft} months left.`);
     
-    
-    
-    
-    
-/*************Don't change the code below**********/
+    /*************Don't change the code below**********/
 }
 
-lifeInWeeks(56)
+lifeInWeeks(56);
